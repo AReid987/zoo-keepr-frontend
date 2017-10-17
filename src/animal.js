16853069331425e5d98ff902d348bde76226fa17
alexgriff <alexalexgriffith@gmail.com>
2017-10-17 13:03:27 -0400

    post lecture

--- src/animal.js
+++ src/animal.js
@@ -1,12 +1,17 @@
 class Animal {
-  constructor(data) {}
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.gender = data.gender;
+    this.speciesId = data.species_id;
+  }
 
   render() {
     return `
     <tr>
       <td>${this.name}</td>
       <td>${this.gender}</td>
-      <td>${this.species}</td>
+      <td>${this.speciesId}</td>
     </tr>
     `;
   }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 $(function() {
   console.log('jquery document ready');
+
+  const $tbody = $('tbody');
+  const $form = $('form.ui.form');
+
+  fetch(`http://localhost:3000/animals/`)
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(animalData => {
+        const animal = new Animal(animalData);
+        $tbody.append(animal.render());
+      });
+    });
+
+  $form.on('submit', ev => {
+    ev.preventDefault();
+    console.log('submitted');
+    const name = $('#animal-name').val();
+    const species = $('#animal-species').val();
+    const gender = $('#animal-gender')
+      .children('.active')
+      .data().value;
+
+    fetch(`http://localhost:3000/animals/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accepts: 'application/json'
+      },
+      body: JSON.stringify({ name, gender, speciesName: species })
+    })
+      .then(res => res.json())
+      .then(json => {
+        const newAnimal = new Animal(json);
+        $tbody.append(newAnimal.render());
+      });
+  });
 });
